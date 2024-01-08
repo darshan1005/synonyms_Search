569f8126fbcbea1f86f620ca1560a3480d10227c
darshan1005 <battuladarshan7838@gmail.com>
2024-01-08 19:18:13 +0530

    flaws updations

--- script.js
+++ script.js
@@ -5,45 +5,44 @@ const synonymsList = document.getElementById('synonymsList');
 synonymsList.innerHTML = 'Synonyms displayed here'
 searchButton.addEventListener('click', searchSynonyms);
 
-function searchSynonyms(e){
+function searchSynonyms(e) {
     const word = searchInput.value.trim();
     e.preventDefault()
-    if(word !== ''){
+    if (word !== '') {
         fetchSynonyms(word);
-    }else{
+    } else {
         clearResults();
     }
 }
 
-function fetchSynonyms(word){
+function fetchSynonyms(word) {
     const apiURL = `https://api.datamuse.com/words?rel_syn=${word}&max=10`;
 
     fetch(apiURL)
-        .then(response =>  response.json())
+        .then(response => response.json())
         .then(data => {
             displaySynonyms(data);
         })
         .catch(error => {
-            console.log('An error occured:',error);
+            console.log('An error occured:', error);
         });
 }
 
-function displaySynonyms(synonyms){
+function displaySynonyms(synonyms) {
     synonymsList.innerHTML = '';
 
-    if(synonyms.length === 0){
-        synonymsList.innerHTML = 'No synonyms found...';
+    if (synonyms.length === 0) {
         return;
+    }else{
+        synonyms.forEach(synonym => {
+            const listItem = document.createElement('li');
+            listItem.textContent = synonym.word;
+            synonymsList.appendChild(listItem);
+        });
     }
+}
 
-    synonyms.forEach(synonym =>{
-        const listItem = document.createElement('li');
-        listItem.textContent = synonym.word;
-        synonymsList.appendChild(listItem);
-    })
-
-    function clearResults(){
-        synonymsList.innerHTML = '';
-        searchInput.value = '';
-    }
+function clearResults() {
+    synonymsList.innerHTML = '';
+    searchInput.value = '';
 }
